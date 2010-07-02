b92e7a56910b6e99d5b2d95cc3f43004103b1e24
Lindsey ONiell <loniell@rothe.cs.utexas.edu>
2010-07-02 15:08:14 -0500

    Date.h now comipiles, still need to fix methods. TestDate.c++ gives 4 failures.

--- Date.h
+++ Date.h
@@ -91,7 +91,7 @@ class Date {
             // <your code>
         	const char *months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
         	if(rhs.valid())
-        		return lhs << rhs.my_day << " " << months[(rhs.my_month -1)] << " " << rhs.my_year << "  -  " << days << " days since 1 Jan 1600" << std::endl;
+        		return lhs << rhs.my_day << " " << months[(rhs.my_month -1)] << " " << rhs.my_year << "  -  " << rhs.days << " days since 1 Jan 1600" << std::endl;
         	else
         		return lhs << "This is not a valid date" << std::endl;
 
@@ -102,10 +102,10 @@ class Date {
         // data
         // ----
         // <your data>
-        static int days;
-        static int my_month;
-        static int my_day;
-        static int my_year;
+        int days;
+        int my_month;
+        int my_day;
+        int my_year;
 
     private:
         // -----
